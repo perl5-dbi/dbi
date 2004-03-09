da464c74bcf90bd3641f6872f1387f0395aba5da
Tim Bunce <tim.bunce@pobox.com>
2004-03-09 15:02:47 +0000

    Prep for 1.42 release candidate 1
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@207 50811bd7-b8ce-0310-adc1-d9db26280581

--- DBI.pm
+++ DBI.pm
@@ -9,7 +9,7 @@
 require 5.006_00;
 
 BEGIN {
-$DBI::VERSION = "1.41"; # ==> ALSO update the version in the pod text below!
+$DBI::VERSION = "1.42"; # ==> ALSO update the version in the pod text below!
 }
 
 =head1 NAME
@@ -118,7 +118,7 @@ Tim he's very likely to just forward it to the mailing list.
 
 =head2 NOTES
 
-This is the DBI specification that corresponds to the DBI version 1.41
+This is the DBI specification that corresponds to the DBI version 1.42
 (C<$Date: 2004/02/01 11:16:16 $>).
 
 The DBI is evolving at a steady pace, so it's good to check that
