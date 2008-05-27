98e77aeb040b82e95c3984b72b2e2a178115c6e5
Tim Bunce <tim.bunce@pobox.com>
2008-05-27 12:27:14 +0000

    Fixed dbixs_revision code in Driver.xst template. Added note re DBIS fix to Changes.
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@11330 50811bd7-b8ce-0310-adc1-d9db26280581

--- dbixs_rev.h
+++ dbixs_rev.h
@@ -1,4 +1,3 @@
-/* Tue May 27 13:18:10 2008 */
-/* Mixed revision working copy (10993M:11328) */
-/* Code modified since last checkin */
-#define DBIXS_REVISION 10993
+/* Tue May 27 13:21:53 2008 */
+/* Mixed revision working copy (11328:11329) */
+#define DBIXS_REVISION 11328
