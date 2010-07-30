8d57ab717d1cded9043f38a5b966cb065c4254f2
Tim Bunce <tim.bunce@pobox.com>
2010-07-30 14:11:52 +0000

    DBIXS_REVISION update
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@14298 50811bd7-b8ce-0310-adc1-d9db26280581

--- dbixs_rev.h
+++ dbixs_rev.h
@@ -1,4 +1,3 @@
-/* Fri Jul 30 14:31:29 2010 */
-/* Mixed revision working copy (14291M:14294) */
-/* Code modified since last checkin */
-#define DBIXS_REVISION 14291
+/* Fri Jul 30 15:11:09 2010 */
+/* Mixed revision working copy (14295:14297) */
+#define DBIXS_REVISION 14295
