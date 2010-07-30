7fc6f8f7fccbc7c247c288b935b5ea88bc4d0660
Tim Bunce <tim.bunce@pobox.com>
2010-07-30 14:00:58 +0000

    Add make ptest target to makefile for parallel testing. Delete dbi*.prof in cleanup. Tweak delays on warnings.
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@14296 50811bd7-b8ce-0310-adc1-d9db26280581

--- dbixs_rev.h
+++ dbixs_rev.h
@@ -1,3 +1,4 @@
-/* Fri Jul 30 14:02:05 2010 */
+/* Fri Jul 30 14:31:29 2010 */
+/* Mixed revision working copy (14291M:14294) */
 /* Code modified since last checkin */
 #define DBIXS_REVISION 14291
