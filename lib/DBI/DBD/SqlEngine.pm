48d1cb026ac3b150eed48dd26e290a70afb716ee
H.Merijn Brand <h.m.brand@xs4all.nl>
2010-09-14 06:10:52 +0000

    fix RT#61281 - bad implementation of bind_param ()
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@14399 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/DBI/DBD/SqlEngine.pm
+++ lib/DBI/DBD/SqlEngine.pm
@@ -744,18 +744,18 @@ sub bind_param ($$$;$)
     if ( $attr && defined $val )
     {
         my $type = ref $attr eq "HASH" ? $attr->{TYPE} : $attr;
-        if (    $attr == DBI::SQL_BIGINT()
-             || $attr == DBI::SQL_INTEGER()
-             || $attr == DBI::SQL_SMALLINT()
-             || $attr == DBI::SQL_TINYINT() )
+        if (    $type == DBI::SQL_BIGINT()
+             || $type == DBI::SQL_INTEGER()
+             || $type == DBI::SQL_SMALLINT()
+             || $type == DBI::SQL_TINYINT() )
         {
             $val += 0;
         }
-        elsif (    $attr == DBI::SQL_DECIMAL()
-                || $attr == DBI::SQL_DOUBLE()
-                || $attr == DBI::SQL_FLOAT()
-                || $attr == DBI::SQL_NUMERIC()
-                || $attr == DBI::SQL_REAL() )
+        elsif (    $type == DBI::SQL_DECIMAL()
+                || $type == DBI::SQL_DOUBLE()
+                || $type == DBI::SQL_FLOAT()
+                || $type == DBI::SQL_NUMERIC()
+                || $type == DBI::SQL_REAL() )
         {
             $val += 0.;
         }
