83ccb17267aed13c0be006a98d00a77ae5e97caf
Tim Bunce <tim.bunce@pobox.com>
2013-03-22 20:31:22 +0000

    Fixed Gofer for hash randomization in perl 5.17.10+ RT#84146
    git-svn-id: https://svn.perl.org/modules/dbi/trunk@15585 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/DBI/Gofer/Serializer/Storable.pm
+++ lib/DBI/Gofer/Serializer/Storable.pm
@@ -46,6 +46,7 @@ use base qw(DBI::Gofer::Serializer::Base);
 sub serialize {
     my $self = shift;
     local $Storable::forgive_me = 1; # for CODE refs etc
+    local $Storable::canonical = 1; # for go_cache
     my $frozen = nfreeze(shift);
     return $frozen unless wantarray;
     return ($frozen, $self->{deserializer_class});
