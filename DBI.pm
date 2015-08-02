e2ec3a3ab60c981f69b024ef802437e9912f7fd6
Tim Bunce <Tim.Bunce@pobox.com>
2015-08-02 17:48:35 +0100

    Rework support for 64bit row counts to rely on macros only. Bump to 1.633_93

--- DBI.pm
+++ DBI.pm
@@ -11,7 +11,7 @@ package DBI;
 require 5.008_001;
 
 BEGIN {
-our $XS_VERSION = our $VERSION = "1.633_92"; # ==> ALSO update the version in the pod text below!
+our $XS_VERSION = our $VERSION = "1.633_93"; # ==> ALSO update the version in the pod text below!
 $VERSION = eval $VERSION;
 }
 
@@ -146,7 +146,7 @@ sure that your issue isn't related to the driver you're using.
 
 =head2 NOTES
 
-This is the DBI specification that corresponds to DBI version 1.633_92
+This is the DBI specification that corresponds to DBI version 1.633_93
 (see L<DBI::Changes> for details).
 
 The DBI is evolving at a steady pace, so it's good to check that
