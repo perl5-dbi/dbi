af7eac9e702d8e770120d28a164eca9701bc2b8b
Tim Bunce <Tim.Bunce@pobox.com>
2018-03-19 17:10:01 +0000

    Bump version, update Changes

--- DBI.pm
+++ DBI.pm
@@ -11,7 +11,7 @@ package DBI;
 require 5.008_001;
 
 BEGIN {
-our $XS_VERSION = our $VERSION = "1.640"; # ==> ALSO update the version in the pod text below!
+our $XS_VERSION = our $VERSION = "1.641"; # ==> ALSO update the version in the pod text below!
 $VERSION = eval $VERSION;
 }
 
@@ -143,7 +143,7 @@ sure that your issue isn't related to the driver you're using.
 
 =head2 NOTES
 
-This is the DBI specification that corresponds to DBI version 1.640
+This is the DBI specification that corresponds to DBI version 1.641
 (see L<DBI::Changes> for details).
 
 The DBI is evolving at a steady pace, so it's good to check that
